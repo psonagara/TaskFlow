2337a3d75619cbcfc9b6a0207444a1384b6aa10c
psonagara <pareshssonagara@gmail.com>
2026-04-10 18:47:18 +0530

    Minor Changes

--- TaskFlow/src/main/java/com/ps/config/CorsConfig.java
+++ TaskFlow/src/main/java/com/ps/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.ps.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,12 +11,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
+	
+	@Value("${taskflow.cors.allowed.origins}")
+	private List<String> allowedOrigin;
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 
 	    CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowedOrigins(List.of("*"));
+	    config.setAllowedOrigins(allowedOrigin);
 	    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 	    config.setAllowedHeaders(List.of("*"));
 	    config.setAllowCredentials(false);
